69318895b4f3f9e6ae11a05aed77780437a3fa28
Vinicius <viniciuusserra1998@outlook.com>
2023-06-19 22:19:57 -0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <v-app class="grey lighten-4 h-screen">
+    <Header />
+    <router-view />
+  </v-app>
+
   <router-view />
 </template>
-
-<script setup>
-  //
+<script>
+import Header from './layouts/header.vue';
+export default {
+  components: {
+    Header
+  }
+}
 </script>
---
+++ src/layouts/header.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <v-toolbar flat app color="">
+        <v-tool-bar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Administrador</span>
+        </v-tool-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat>
+            <v-icon color="light-blue-accent-3" size="x-large">mdi-account-circle</v-icon>
+        </v-btn>
+
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+.header {
+    grid-area: headers;
+    height: 20%;
+}
+</style>
